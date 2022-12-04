5816868d64798f6c47ee02d153669e64b547276f
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-03 22:08:39 -0800

    Added initial header item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './components/Foooter';
+import Header from './components/Header';
 
 function App() {
   return(
     <div>
         {/* Add Header */}
+        <Header/>
         {/* Add Projects */}
         {/* Add Footer */}
         <Footer/>
--- src/components/Foooter.js
+++ src/components/Foooter.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer class="">
-            <a class="mx-3 footer-item" href="https://github.com/Angellyn218">Github</a>
-            <a class="mx-3 footer-item" href="https://www.linkedin.com/in/angel-cervantes-294449167/">LinkedIn</a>
-            <a class="mx-3 footer-item" href="https://stackoverflow.com/users/20678820/angel-cervantes">Stack Overflow</a>
+        <footer className="">
+            <a className="mx-3 footer-item" href="https://github.com/Angellyn218">Github</a>
+            <a className="mx-3 footer-item" href="https://www.linkedin.com/in/angel-cervantes-294449167/">LinkedIn</a>
+            <a className="mx-3 footer-item" href="https://stackoverflow.com/users/20678820/angel-cervantes">Stack Overflow</a>
         </footer>
     )
 }
