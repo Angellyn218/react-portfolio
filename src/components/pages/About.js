1cc16e5638937c126e8c83a1d573856383adc378
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-04 14:30:55 -0800

    Added pages and completed initial about section

---
+++ src/components/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div className="page">
+            <h1>About me</h1> 
+            <p>
+                Angel Cervantes is a student who was born and raised in the San Francisco Bay Area. She earned her GED at “Waldorf School of the Peninsula,” graduating in 2020, 
+                and started her studies at the “University of British Columbia” that same year. After realizing her love of web development, she took a year of school to focus 
+                on her passion for it but plans to return to college in the fall of 2023.
+                <br/>
+                <br/>
+                In addition to coding, she loves to dance. She has danced Hawaiian Hula Dance from the ages of 4 to 18. Starting at 8, she participated in numerous Hula 
+                competitions alongside her cohorts, winning on multiple occasions. Another style of dancing that she has also studied is hip hop. She started in high school as 
+                a sophomore. She would join a local group called “Krazy8”, a hip hop competition group, in her senior year. She moved into “Str8jacket”, the adult version of 
+                “Krazy8”, a year later. She has been dancing with them since returning to the Bay Area.
+            </p>
+        </div>
+    )
+}
