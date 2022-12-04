e35785cf2eed281f8212061e8045180241a1ae75
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-04 15:42:30 -0800

    Added contact page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,38 @@
 import React, { useState } from "react";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function Header() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#about">About me</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#portfolio">Portfolio</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#contact">Contact</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link disabled" href="#resume">Resume</a>
-                    </li>
-                </ul>
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <h1 className="navbar-brand" href="#">Navbar</h1>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#about">About me</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link disabled" href="#resume">Resume</a>
+                        </li>
+                    </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <br/>
+            <About/>
+            <br/>
+            <Contact/>
+        </div>
     )
 }
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Contact() {
+    return (
+        <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name:</label>
+                <input type="text" class="form-control" id="name" placeholder="John Doe"/>
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address:</label>
+                <input type="email" class="form-control" id="email" placeholder="john-doe@email.com"/>
+            </div>
+            <div class="mb-3">
+                <label for="message" class="form-label">Message:</label>
+                <textarea class="form-control" id="message" rows="3" placeholder="Write message"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    )
+}
