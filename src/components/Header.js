fcee0b9e5c367bc5f59229f0f95af35b87b011d6
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-04 23:16:23 -0800

    Added className of page to contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import Navigation from "./Navigation";
 export default function Header() {
     return (
         <div>
+            <h1>Angel Cervantes</h1>
             <Navigation/>
         </div>
     )
