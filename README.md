24ecf2f4029b5c32aca91762b31a342319204dd9
Angellyn218 <angellyncervantes@yahoo.com>
2022-12-05 22:18:02 -0800

    Changed Project and added credit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# react-portfolio
+# React Portfolio
+
+## Credits
+add image to react: https://stackoverflow.com/questions/37644265/correct-path-for-img-on-react-js
--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Project(props) {
     return (
         <div className="card">
-            <img src={require(window.location.origin + props.image)} className="card-img-top" alt={props.alt}/>
+            <img src={props.image} className="card-img-top" alt={props.alt}/>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.tech}</p>
