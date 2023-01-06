df66121fc63da048ab56c81ca5b555d6b62c432b
Angellyn218 <angellyncervantes@yahoo.com>
2023-01-05 16:36:59 -0800

    Added document title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import { useEffect } from 'react';
 import Footer from './components/Foooter';
 import Header from './components/Header';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Angel Cervantes';
+  }, []);
+
   return(
     <div className="app">
         <Header/>
