8c62779a794e7026020669b40a1d870b20dd305a
Angellyn218 <angel.lyn.cervantes@gmail.com>
2024-04-25 17:16:13 -0700

    edited about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,10 @@ export default function About() {
             <h1>About me</h1> 
             <img src={ProfilePicture} className="profile" alt="profile" />
             <p>
-                Angel Cervantes is a student who was born and raised in the San Francisco Bay Area. She earned her GED at “Waldorf School of the Peninsula,” graduating in 2020, 
+                Angel Cervantes is a junior at California State University Fullerton. She was born and raised in the San Francisco Bay Area. She earned her diploma at “Waldorf School of the Peninsula,” graduating in 2020, 
                 and started her studies at the “University of British Columbia” that same year. After realizing her love of web development, she took a year of school to focus 
-                on her passion for it but plans to return to college in the fall of 2023.
+                on her passion for it. She spent the year taking a web development bootcamp with the UC Berkeley Extension Program and taking the GEs needed to transfer to a California College. 
+                She successfully transferred to California State University, Fullerton in Fall 2023. 
                 <br/>
                 <br/>
                 In addition to coding, she loves to dance. She has danced Hawaiian Hula Dance from the ages of 4 to 18. Starting at 8, she participated in numerous Hula 
